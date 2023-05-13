be6d7f9822c38f0d0978653675b746947cbabb14
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-13 19:21:38 +0100

    readme review and correction

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,7 +12,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		if (tree->left == NULL && tree->right == NULL00)
+		if (tree->left == NULL && tree->right == NULL)
 		{
 			return (1);
 		}
