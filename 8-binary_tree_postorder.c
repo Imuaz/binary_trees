ff826c8fbbc39badeb214d5d894a87f5a72d1399
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-13 16:13:52 +0100

    readme review and correction

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -12,9 +12,10 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		return;
 	}
+	/*Recursively traverse the left subtree*/
 	binary_tree_postorder(tree->left, func);
-
+	/*Recursively traverse the right subtree*/
 	binary_tree_postorder(tree->right, func);
-
+	/*Call the function for the current node*/
 	func(tree->n);
 }
