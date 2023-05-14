2021ed72ca37f3ee4180661791dbe99c9eed82a6
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-14 18:57:12 +0000

    readme review and correction

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -11,16 +11,22 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 	if (tree == NULL || tree->right == NULL)
 		return (NULL);
 
-	binary_tree_t *new_root = tree->right;
-	binary_tree_t *subtree = new_root->left;
-	/*Perform the rotation*/
-	new_root->left = tree;
-	tree->right = subtree;
-	/*Update parent pointers*/
-	if (subtree != NULL)
-		subtree->parent = tree;
-	new_root->parent = tree->parent;
-	tree->parent = new_root;
+	binary_tree_t *pivot = tree->right;
+	binary_tree_t *tmp = pivot->left;
 
-	return (new_root);
+	pivot->left = tree;
+	tree->right = tmp;
+	if (tmp != NULL)
+		tmp->parent = tree;
+	tmp = tree->parent;
+	tree->parent = pivot;
+	pivot->parent = tmp;
+	if (tmp != NULL)
+	{
+		if (tmp->left == tree)
+			tmp->left = pivot;
+		else
+			tmp->right = pivot;
+	}
+	return (pivot);
 }
