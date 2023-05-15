95acbe2b62cf7b32363ba4bd48933feebe605452
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-15 22:17:39 +0000

    readme review and correction

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -1,5 +1,21 @@
 #include "binary_trees.h"
 
+avl_t *sorted_array_to_avl_helper(int *array, int start, int end);
+/**
+ * sorted_array_to_avl - Builds an AVL tree from a sorted array.
+ * @array: Pointer to the first element of the array.
+ * @size: Number of elements in the array.
+ * Return: Pointer to the root node of the created AVL tree,
+ * or NULL on failure
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	if (array == NULL || size == 0)
+		return (NULL);
+
+	return (sorted_array_to_avl_helper(array, 0, size - 1));
+}
+
 /**
  * sorted_array_to_avl_helper - Helper function to build AVL tree
  * from sorted array.
@@ -13,11 +29,11 @@ avl_t *sorted_array_to_avl_helper(int *array, int start, int end)
 	int mid;
 	avl_t *node;
 
-	if (start > end)
-		return (NULL);
-
 	mid = (start + end) / 2;
 	node = malloc(sizeof(avl_t));
+
+	if (start > end)
+		return (NULL);
 
 	if (node == NULL)
 		return (NULL);
@@ -26,19 +42,10 @@ avl_t *sorted_array_to_avl_helper(int *array, int start, int end)
 	node->left = sorted_array_to_avl_helper(array, start, mid - 1);
 	node->right = sorted_array_to_avl_helper(array, mid + 1, end);
 
+	if (node->left != NULL)
+		node->left->parent = node;
+	if (node->right != NULL)
+		node->right->parent = node;
+
 	return (node);
-}
-
-/**
- * sorted_array_to_avl - Builds an AVL tree from a sorted array.
- * @array: Pointer to the first element of the array.
- * @size: Number of elements in the array.
- * Return: Pointer to the root node of the created AVL tree, or NULL on failure
- */
-avl_t *sorted_array_to_avl(int *array, size_t size)
-{
-	if (array == NULL || size == 0)
-		return (NULL);
-
-	return (sorted_array_to_avl_helper(array, 0, size - 1));
 }
