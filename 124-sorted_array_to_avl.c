d2f67c3ff9abbb6a97448de9830383172ea85baf
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-16 05:47:00 +0000

    readme review and correction

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 
-avl_t *sorted_array_to_avl_helper(int *array, int start, int end);
+avl_t *sorted_array_to_avl_helper(int *arr, int start, int end, avl_t *parent);
+
 /**
  * sorted_array_to_avl - Builds an AVL tree from a sorted array.
  * @array: Pointer to the first element of the array.
@@ -13,39 +14,35 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 	if (array == NULL || size == 0)
 		return (NULL);
 
-	return (sorted_array_to_avl_helper(array, 0, size - 1));
+	return (sorted_array_to_avl_helper(array, 0, size - 1, NULL));
 }
 
 /**
- * sorted_array_to_avl_helper - Helper function to build AVL tree
- * from sorted array.
- * @array: Pointer to the first element of the array.
- * @start: Index of the start element of the current subarray.
- * @end: Index of the end element of the current subarray.
- * Return: Pointer to the root node of the created AVL tree, or NULL on failure
+ * sorted_array_to_avl_helper - Helper function for sorted_array_to_avl
+ * @arr: Input array
+ * @start: Starting index
+ * @end: Ending index
+ * @parent: Pointer to parent node
+ o* Return: Newly created node
  */
-avl_t *sorted_array_to_avl_helper(int *array, int start, int end)
+avl_t *sorted_array_to_avl_helper(int *arr, int start, int end, avl_t *parent)
 {
-	int mid;
-	avl_t *node;
-
-	mid = (start + end) / 2;
-	node = malloc(sizeof(avl_t));
+	int middle;
+	avl_t *new_node;
 
 	if (start > end)
 		return (NULL);
 
-	if (node == NULL)
+	middle = (start + end) / 2;
+	new_node = malloc(sizeof(avl_t));
+
+	if (new_node == NULL)
 		return (NULL);
 
-	node->n = array[mid];
-	node->left = sorted_array_to_avl_helper(array, start, mid - 1);
-	node->right = sorted_array_to_avl_helper(array, mid + 1, end);
+	new_node->n = arr[middle];
+	new_node->parent = parent;
+	new_node->left = sorted_array_to_avl_helper(arr, start, middle - 1, new_node);
+	new_node->right = sorted_array_to_avl_helper(arr, middle + 1, end, new_node);
 
-	if (node->left != NULL)
-		node->left->parent = node;
-	if (node->right != NULL)
-		node->right->parent = node;
-
-	return (node);
+	return (new_node);
 }
